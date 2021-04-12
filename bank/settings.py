6d640edf5787ec4ae9f882b7c8ab2c2533cdd5c5
isaiah <daezyworld@gmail.com>
2021-04-12 08:39:29 +0100

    updated allowed host

--- bank/settings.py
+++ bank/settings.py
@@ -159,13 +159,9 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media_cdn')
 
 
 #Email config
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-#EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'mydappy@gmail.com'
-EMAIL_HOST_PASSWORD = 'urnznsbpztnprhqk'
+#EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
 
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
